9f8b5c46d61f2d838f167c23c8b4149e0fb3e210
Maarten van Gompel <proycon@anaproy.nl>
2020-10-19 17:54:27 +0200

    v1.5

--- forcedalignment2/resources/run_forced_alignment.sh
+++ forcedalignment2/resources/run_forced_alignment.sh
@@ -118,6 +118,8 @@ if [ $stage -le 2 ]; then
 
 			#Preparing wav.scp, segments, utt2spk, spk2utt text file and dictionary for $filename "..."
 			${python_cmd} data/local/data_prep.py --align_tier_name $align_tier_name --speaker_adapt SA --wav_file $filename --annot_folder ${main_folder_wav} --data_folder $datadir --dict_file ~/clst-asr-fa/lexicon.txt
+
+
 			
 			#Calculating MFCC features from" + filename + "..."
 			mkdir -p $datadir/mfcc
@@ -128,9 +130,60 @@ if [ $stage -le 2 ]; then
 			utils/fix_data_dir.sh $datadir
 
 			# Preparing language resources directory" + filename + "..."
-			utils/prepare_lang.sh --sil_prob 0.5 --position-dependent-phones true --num-sil-states 3 ${datadir}/dict "<UNK>" ${datadir}/tmp ${datadir}/lang
+			utils/prepare_lang.sh --sil_prob 0.05 --position-dependent-phones true --num-sil-states 3 ${datadir}/dict "<UNK>" ${datadir}/tmp ${datadir}/lang
+
 			# Create simple linear FST LM for better alignment
 			${python_cmd} data/local/lang_prep_fst-lm.py --align_tier_name $align_tier_name --wav_file $filename --annot_folder ${main_folder_wav} --data_folder $datadir --use_word_int_ids --dict_file ~/clst-asr-fa/lexicon.txt || continue
+
+			# this creates a textfile G.fst.txt in $datadir/lang
+			# of which the first line does not belong to the actual FST
+
+			echo SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+			ls $datadir/lang/G.fst.txt
+			cat $datadir/lang/G.fst.txt
+			ls $datadir/lang/L.fst
+
+
+			# replace previous command by a cascade of smaller steps
+			# 0: start from .wav filename ($filename)
+			# 1: create .tg name from .wav filename, forget about "-16khz" tag
+			intgname=`echo $filename | sed 's/\-16khz//' | sed 's/.wav/.tg/'`
+
+			# 2: get path name of .wav file (use same for .tg)
+			# 3: get orthography from .tg
+			# assume the orthography resides on last line between double quotes
+                        echo orthography read from file $intgname
+			ortho=`cat $intgname | tail -1 | perl -ne 'm/\"(.*)\"/; printf("%s\n", $1);'`
+			echo distilled ortho: $ortho
+			workingdir=$datadir
+			echo workingdir $workingdir
+			scriptdir=/home/ltenbosch/perl
+			scriptdir2="/home/ltenbosch/BNF2LATTICE/step1"
+			echo $ortho | perl $scriptdir/ortho2BNF.pl > $workingdir/BNF.txt
+			# echo tim nam | perl $scriptdir/ortho2BNF.pl > $workingdir/BNF.txt
+
+			# 4: build word-based FST from orthography
+                        echo workingdir/BNF.txt $workingdir/BNF.txt
+			perl $scriptdir2/bnf_step1_v6.pl $workingdir/BNF.txt > $workingdir/FST_word.txt
+                        echo workingdir/FST_word.txt $workingdir/FST_word.txt
+			cat $workingdir/FST_word.txt
+                        # 5: transform word-based FST to integer-based FST
+			#ls ${datadir}/lang/words.txt
+			echo TTTTTTTTTTTTTTTTTTTTTT
+			head -20 ${datadir}/lang/words.txt
+			
+			cat $workingdir/FST_word.txt | perl $scriptdir/FST_word2int.pl ${datadir}/lang/words.txt > $workingdir/FST_int.txt
+			echo output $workingdir/FST_int.txt
+			cat $workingdir/FST_int.txt
+
+			# recreate $datadir/lang/G.fst.txt by overwriting
+			mv $datadir/lang/G.fst.txt  $datadir/lang/G.fst.txt_orig
+			cat $datadir/lang/G.fst.txt_orig | head -1 > $datadir/lang/G.fst.txt 
+			cat $workingdir/FST_int.txt >> $datadir/lang/G.fst.txt
+			# check:
+			# cat $datadir/lang/G.fst.txt
+			
+			ls $datadir/lang/L_disambig.fst
 
 			# Starting the actual process of aligning the transcriptions to audio..."
 			steps/online/nnet2/align.sh --beam 5 --retry-beam 100 --cmd "$train_cmd" --nj 1 $datadir ${datadir}/lang $acmod $aligndir/$filename_only || (echo -e "ERROR: Could not decode ${filename}!\nERROR: Look at log in $aligndir/$filename_only/log/align.1.log.");
