7d7f410b19946f701e367d2f92cbe6b443c8cc23
Maarten van Gompel <proycon@anaproy.nl>
2020-10-20 15:15:52 +0200

    added a --force option that omits the git check

--- resources/download_resources.sh
+++ resources/download_resources.sh
@@ -3,13 +3,15 @@
 # this scripts build the resource tarball when the resources file are presented
 # in the directory structure (but untracked by git)
 
-GITROOT=$(git rev-parse --show-toplevel)
-if [ -z "$GITROOT" ]; then
-    echo "not in a git repository!"
-    exit 2
-fi
+if [ "$1" != "--force" ]; then
+    GITROOT=$(git rev-parse --show-toplevel)
+    if [ -z "$GITROOT" ]; then
+        echo "not in a git repository!"
+        exit 2
+    fi
 
-cd $GITROOT/resources
+    cd $GITROOT/resources
+fi
 
 rm forcedalignment2_resources.tar.gz 2>/dev/null
 wget https://applejack.science.ru.nl/downloads/forcedalignment/forcedalignment2_resources.tar.gz && tar -xvzf forcedalignment2_resource.tar.gz
